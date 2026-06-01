5b10589d6941132fcdc18a8deff874f36fb3c40a
songzh <haskell.songzh@hotmail.com>
2026-06-01 21:06:55 +0800

    remove FirExtensionRegistar

--- compiler-plugin/src/dev/songzh/function/trace/SimplePluginComponentRegistrar.kt
+++ compiler-plugin/src/dev/songzh/function/trace/SimplePluginComponentRegistrar.kt
@@ -3,9 +3,7 @@ package dev.songzh.function.trace
 import org.jetbrains.kotlin.backend.common.extensions.IrGenerationExtension
 import org.jetbrains.kotlin.compiler.plugin.CompilerPluginRegistrar
 import org.jetbrains.kotlin.config.CompilerConfiguration
-import org.jetbrains.kotlin.fir.extensions.FirExtensionRegistrarAdapter
 import dev.songzh.function.trace.ir.FunctionTracerIrGenerationExtension
-import dev.songzh.function.trace.ir.SimpleIrGenerationExtension
 
 /** Default package shipped with plugin-annotations. */
 private const val DEFAULT_PACKAGE_PATH = "dev.songzh.function.trace"
@@ -21,8 +19,6 @@ class SimplePluginComponentRegistrar : CompilerPluginRegistrar() {
             _root_ide_package_.dev.songzh.function.trace.DEFAULT_PACKAGE_PATH
         )
 
-        FirExtensionRegistrarAdapter.registerExtension(_root_ide_package_.dev.songzh.function.trace.SimplePluginRegistrar())
-        IrGenerationExtension.registerExtension(_root_ide_package_.dev.songzh.function.trace.ir.SimpleIrGenerationExtension())
         IrGenerationExtension.registerExtension(
             _root_ide_package_.dev.songzh.function.trace.ir.FunctionTracerIrGenerationExtension(
                 traceAll = traceAll,
