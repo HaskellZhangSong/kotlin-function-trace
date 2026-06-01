2dc51cc77b795b237cdecbe60e6442432541dcb6
songzh <haskell.songzh@hotmail.com>
2026-06-01 17:10:59 +0800

    add publish plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     kotlin("jvm") version "2.1.20" apply false
     id("com.github.gmazzo.buildconfig") version "5.6.5"
     id("org.jetbrains.kotlinx.binary-compatibility-validator") version "0.16.3" apply false
+    id("maven-publish")
 }
 
 allprojects {
