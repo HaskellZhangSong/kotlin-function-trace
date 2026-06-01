29977c926db9fd1022c543b71ab82bc0c619457e
songzh <haskell.songzh@hotmail.com>
2026-06-01 20:53:23 +0800

    add maven local and change project name

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -4,18 +4,22 @@ pluginManagement {
         gradlePluginPortal()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
     }
-    
+
 }
+
 
 dependencyResolutionManagement {
     repositories {
+        mavenCentral()
+        mavenLocal()   // picks up plugin-annotations
         mavenCentral()
         maven("https://maven.pkg.jetbrains.space/kotlin/p/kotlin/bootstrap")
     }
 }
 
-rootProject.name = "kotlin-compiler-plugin-template"
+rootProject.name = "kotlin-function-tracer"
 
 include("compiler-plugin")
 include("gradle-plugin")
 include("plugin-annotations")
+
